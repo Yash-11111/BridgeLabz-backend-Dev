e0ec319dd5bc5356fe8742235afde49727d9a8ff
Yash-11111 <yashkesarwani8040@gmail.com>
2026-01-27 11:15:05 +0530

    Q&A

--- file_system/assignment/q1.js
+++ file_system/assignment/q1.js
@@ -1,25 +1,25 @@
  const fs = require("fs");
 
 
-// // const re = fs.readFileSync("./quote.txt","utf-8");
+const re = fs.readFileSync("./quote.txt","utf-8");
 
 
-// // fs.writeFileSync("./q1.txt","hello ji!");
+fs.writeFileSync("./q1.txt","hello ji!");
 
 
 
 
-// // fs.appendFileSync("./q1.txt","./q1_copy.txt");
+fs.appendFileSync("./q1.txt","./q1_copy.txt");
 
-// // fs.unlinkSync("./q1_copy.txt");
+fs.unlinkSync("./q1_copy.txt");
 
-// // fs.readdir("./",(err,files)=>{
-// //     if(err){
-// //         console.log("error",err);
-// //     }else{
-// //         console.log("files",files);
-// //     }
-// // })
+fs.readdir("./",(err,files)=>{
+    if(err){
+        console.log("error",err);
+    }else{
+        console.log("files",files);
+    }
+})
 
 
 fs.writeFileSync("./answer.txt","Questions & Answers!\n \n ");
